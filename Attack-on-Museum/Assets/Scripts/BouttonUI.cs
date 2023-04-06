3d6f905ee596e2283b51ece57046bbb495b2b141
ShohoPok <vazgen-41@hotmail.fr>
2023-04-06 13:27:09 +0200

    bouton Fini

--- Attack-on-Museum/Assets/Scripts/BouttonUI.cs
+++ Attack-on-Museum/Assets/Scripts/BouttonUI.cs
@@ -8,13 +8,15 @@ public class BouttonUI : MonoBehaviour
     [SerializeField] private string _newGameLevel = "Level1";
     [SerializeField] private GameObject _menuCredits = null;
     [SerializeField] private GameObject _mainMenu = null;
-    
+    [SerializeField] private GameObject _menuPause = null;
+    private bool _isPaused = false;
+
 
 
     public void NewGameButton()
     {
         SceneManager.LoadScene(_newGameLevel);
-       
+
     }
 
     public void BoutonQuit()
@@ -25,10 +27,43 @@ public class BouttonUI : MonoBehaviour
     public void Credits()
     {
         _menuCredits.SetActive(true);
+        _mainMenu.SetActive(false);
     }
 
     public void MainMenu()
     {
         _mainMenu.SetActive(true);
     }
+
+    public void Pause()
+    {
+        _menuPause.SetActive(true);
+        Time.timeScale = 0;
+        _isPaused = true;
+    }
+
+    public void Resume()
+    {
+        _menuPause.SetActive(false);
+        Time.timeScale = 1;
+        _isPaused = false;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if (_isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
 }
+
+
+
