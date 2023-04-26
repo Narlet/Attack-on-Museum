594fe8ed92a322235c7287d37c3bf63dccc3f779
Manyzeur <hugo-machado@outlook.fr>
2023-04-26 09:17:10 +0200

    [LVLDESIGN] Correction bug

---
+++ Attack-on-Museum/Assets/Scripts/DebugSceneLoader.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DebugSceneLoader : MonoBehaviour
+{
+    void Awake()
+    {
+        TimeManager.Instance.GameOver = false;
+        TimeManager.Instance.Paused = false;
+    }
+
+}
