7b458c96095f4fcd933e364717e5dd725d928792
ShohoPok <vazgen-41@hotmail.fr>
2023-04-28 15:48:36 +0200

    sound

--- Attack-on-Museum/Assets/Scripts/BoutonMainMenu.cs
+++ Attack-on-Museum/Assets/Scripts/BoutonMainMenu.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class BoutonMainMenu : MonoBehaviour
 {
     [SerializeField] private string _newGameLevel = "GameLevel";
     [SerializeField] private string _mainMenu = " MainMenu";
     [SerializeField] private GameObject _menuCredits = null;
-
+    [SerializeField] private AudioSource _audioClick = null;
+    
 
    
 
 
     public void NewGameButton()
     {
+        
         SceneManager.LoadScene(_newGameLevel);
     }
 
@@ -28,15 +31,23 @@ public class BoutonMainMenu : MonoBehaviour
 
     public void Credits()
     {
+        
         _menuCredits.SetActive(true);
 
     }
 
     public void MainMenu()
     {
+     
         _menuCredits.SetActive(false);
 
     }
+
+    public void TaskOnClick()
+    {
+        _audioClick.Play();
+    }
+    
 
 
 }
